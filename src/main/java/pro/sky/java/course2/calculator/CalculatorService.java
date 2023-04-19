994776f7f658039f7f9c96b38c7cba96089005a7
dariamilk <daryachulpanova@gmail.com>
2023-04-19 22:42:22 +0300

    ДЗ урока 2.3

---
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorService.java
@@ -0,0 +1,9 @@
+package pro.sky.java.course2.calculator;
+
+public interface CalculatorService {
+    String greetings();
+    String plus(int num1, int num2);
+    String minus(int num1, int num2);
+    String multiply(int num1, int num2);
+    String divide(int num1, int num2);
+}
