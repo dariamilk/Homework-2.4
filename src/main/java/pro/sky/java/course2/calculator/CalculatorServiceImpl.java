9cfa78a73c94018698bc6bc3519ff13b6b938ebd
dariamilk <daryachulpanova@gmail.com>
2023-06-16 22:06:16 +0300

    ДЗ урока 2.11

--- src/main/java/pro/sky/java/course2/calculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorServiceImpl.java
@@ -9,23 +9,23 @@ public class CalculatorServiceImpl implements CalculatorService {
         return "Добро пожаловать в калькулятор!";
     }
     @Override
-    public String plus(int num1, int num2) {
-        return num1 + " + " + num2 + " = " + (num1 + num2);
+    public int plus(int num1, int num2) {
+        return num1 + num2;
      }
     @Override
-    public String minus(int num1, int num2) {
-        return num1 + " - " + num2 + " = " + (num1 - num2);
+    public int minus(int num1, int num2) {
+        return num1 - num2;
     }
     @Override
-    public String multiply(int num1, int num2) {
-        return num1 + " * " + num2 + " = " + (num1 * num2);
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
     }
     @Override
-    public String divide(int num1, int num2) {
+    public double divide(int num1, int num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("На ноль делить нельзя");
         } else {
-            return num1 + " / " + num2 + " = " + ((double)num1 / (double)num2);
+            return (double)num1 / (double)num2;
         }
     }
 }
